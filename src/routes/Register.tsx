7058520c2aefe530f5d7eb204d519fe2ac8f1cfa
danattali <danattali242@gmail.com>
2024-03-19 14:33:57 +0200

    New

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,18 +1,30 @@
 import { useForm } from "react-hook-form";
+import { RegisterUser } from "../@types/types";
 
 const Register = () => {
 
-    const { register, handleSubmit } = useForm();
+    const { register,
+        handleSubmit,
+        formState: { errors } } = useForm<RegisterUser>();
 
-    const onRegister = (data) => {
+    const onRegister = (data: RegisterUser) => {
         console.log(data);
     };
 
     return (
-        <form onSubmit={handleSubmit(onRegister)}>
-            <input type="text" {...register("firstname")} />
-            <input type="text" {...register("lastname")} />
-            <input type="number" {...register("age")} />
+        <form noValidate onSubmit={handleSubmit(onRegister)}>
+            <input type="text" {...register("name.first", {
+                required: "this field is mandatory",
+                minLength: { value: 2, message: "too short" },
+                maxLength: { value: 255, message: "too long" }
+            })} />
+            {errors.name?.first && <p className="text-red-500">{errors.name.first.message}</p>}
+            <input type="text" {...register("name.last", {
+                required: "this field is mandatory",
+                minLength: { value: 2, message: "too short" },
+                maxLength: { value: 255, message: "too long" }
+            })} />
+            {errors.name?.last && <p className="text-red-500">{errors.name.last.message}</p>}
             <button type="submit">Register</button>
         </form>
     );
