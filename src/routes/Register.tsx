d21f807afc18e18fb1fa711ec78d7b4e388fb967
danattali <danattali242@gmail.com>
2024-03-19 15:14:58 +0200

    new

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -12,21 +12,42 @@ const Register = () => {
     };
 
     return (
-        <form noValidate onSubmit={handleSubmit(onRegister)}>
-            <input type="text" {...register("name.first", {
-                required: "this field is mandatory",
-                minLength: { value: 2, message: "too short" },
-                maxLength: { value: 255, message: "too long" }
-            })} />
-            {errors.name?.first && <p className="text-red-500">{errors.name.first.message}</p>}
-            <input type="text" {...register("name.last", {
-                required: "this field is mandatory",
-                minLength: { value: 2, message: "too short" },
-                maxLength: { value: 255, message: "too long" }
-            })} />
-            {errors.name?.last && <p className="text-red-500">{errors.name.last.message}</p>}
-            <button type="submit">Register</button>
-        </form>
+        <div className="register-container">
+            <h2 className="text-4xl mb-5 text-center">Register</h2>
+            <form noValidate onSubmit={handleSubmit(onRegister)}>
+
+                <input
+                    placeholder="First Name"
+                    type="text" {...register("name.first", {
+                        required: "this field is mandatory",
+                        minLength: { value: 2, message: "too short" },
+                        maxLength: { value: 255, message: "too long" }
+                    })} />
+                {errors.name?.first && <p className="text-red-500">{errors.name.first.message}</p>}
+                <input
+                    placeholder="Middle Name"
+                    type="text"
+                    {...register("name.middle", {
+                        minLength: { value: 2, message: "Too short" },
+                        maxLength: { value: 255, message: "Too long" },
+                    })}
+                />
+                {errors.name?.middle && (
+                    <p className="text-red-500">{errors.name?.middle?.message}</p>
+                )}
+
+                <input
+                    placeholder="Phone"
+                    type="tel" {...register("phone", {
+                        required: "this field is mandatory",
+                        minLength: { value: 9, message: "too short" },
+                        maxLength: { value: 10, message: "too long" },
+                    })} />
+                {errors.phone && <p className="text-red-500">{errors.phone.message}</p>}
+                <button type="submit">Register</button>
+
+            </form>
+        </div >
     );
 };
 
