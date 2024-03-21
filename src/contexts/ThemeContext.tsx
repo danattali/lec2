513b969453f272d2f9f3298f75c39fab0e640a29
danattali <danattali242@gmail.com>
2024-03-21 15:26:35 +0200

    new

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,9 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext } from "react";
 
-export const ThemeContext = createContext("light");
-export function ThemeProvider({ children }) {
-    const [theme, setTheme] = useState("light");
-    return (
-        <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
-    )
-}
+export const ThemeContext = createContext({
+    theme: "light",
+    toggle: () => { }
+});
