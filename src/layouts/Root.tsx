0142f1eaebc53dc9ff9de1c3159e273c90707525
danattali <danattali242@gmail.com>
2024-03-21 15:23:19 +0200

    new roots

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -3,16 +3,25 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useState } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const Root = () => {
+    //state that we will share with the entire app
+    const [theme, setTheme] = useState('light')
+
+
     return (
-        <div className="flex flex-col min-h-screen text-blue-500">
-            <Header />
-            <main className="flex-1">
-                <Outlet />
-            </main>
-            <Footer />
-        </div>
+        <ThemeContext.Provider value={theme}>
+
+            <div className="flex flex-col min-h-screen text-blue-500">
+                <Header />
+                <main className="flex-1">
+                    <Outlet />
+                </main>
+                <Footer />
+            </div>
+        </ThemeContext.Provider >
     );
 };
 
