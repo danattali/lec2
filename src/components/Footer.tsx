2e6e18d69198a3b7412e7b68d0e0884b08cff67a
danattali <danattali242@gmail.com>
2024-03-21 15:46:30 +0200

    new option

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
 
 const Footer = () => {
+    const { toggle } = useContext(ThemeContext)
     return (
-        <footer className="bg-slate-700 p-5 md:bg-orange-600 text-blue-500 text-5xl  font-extralight text-center">footer © 2024</footer>
+        <footer className="bg-slate-700 p-5 md:bg-orange-600 text-blue-500 text-5xl  font-extralight text-center">
+            <button onClick={() => toggle()}>click me</button>
+            footer © 2024
+
+        </footer>
     )
 }
 
