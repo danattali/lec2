d53660550a2122fd1cb89b65551d9caa0082969d
danattali <danattali242@gmail.com>
2024-03-21 14:15:51 +0200

    new login page

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,23 +1,50 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { LoginUser } from '../@types/types'
+import patterns from '../validation/patterns'
+import auth from '../services/auth'
+import { data } from 'autoprefixer'
+import dialogs from '../ui/dialogs'
 
 const Login = () => {
-    const onLogin = (data: loginUser) => { }
+    const onLogin = (data: LoginUser) => {
+        auth
+            .login(data)
+            .then((res) => {
+                console.log(res.data);
+
+            })
+            .catch((e) => {
+                dialogs.error('Login Error', e.response.data)
+            });
+    }
 
     const { register, handleSubmit, formState: { errors } } = useForm<LoginUser>()
     return (
         <div>
             <h2>Login Page</h2>
-            <form onSubmit={handleSubmit(onLogin)}>
+            <form noValidate onSubmit={handleSubmit(onLogin)}>
                 {/*email*/}
-                <section></section>
+                <section>
+                    <input type="email" placeholder='Email'
+                        {...register('email', {
+                            required: 'Email is required',
+                            pattern: patterns.email
+                        })}
+                    />
+                    {errors.email && <p>{errors.email?.message}</p>}
+                </section>
                 {/*password*/}
-                <section></section>
-
-                <button>Login</button>
-
-
+                <section>
+                    <input type="password" placeholder='Password'
+                        {...register('password', {
+                            required: 'Password is required',
+                            pattern: patterns.password
+                        })
+                        }
+                    />
+                    {errors.password && <p>{errors.password?.message}</p>}
+                </section>
                 <button type="submit">Login</button>
             </form>
         </div >
