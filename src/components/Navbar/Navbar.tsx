4ddb5558e4692ede4176a08d02665c50457faf39
danattali <danattali242@gmail.com>
2024-03-26 14:47:34 +0200

    new

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,27 @@
 import { NavLink } from "react-router-dom";
 import "./Navbar.scss";
-import { FaHome } from 'react-icons/fa';
+import { FaHome } from "react-icons/fa";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const Navbar = () => {
+    const abc = () => "abc";
+    const { isLoggedIn, logout } = useContext(AuthContext);
+
     return (
         <nav className="site-navbar">
             <div className="nav-left">
-                Home
-                <NavLink to="/">
+                <NavLink to="/" className="brand">
+                    Home
                     <FaHome />
                 </NavLink>
             </div>
 
             <div className="nav-right">
-                <NavLink to="/register">Register</NavLink>
-                <NavLink to="/login">Login</NavLink>
+                {!isLoggedIn && <NavLink to="/register">Register</NavLink>}
+                {!isLoggedIn && <NavLink to="/login">Login</NavLink>}
+                {isLoggedIn && <button onClick={logout}>Logout</button>}
                 <DarkModeToggle />
             </div>
         </nav>
