6352f81a47d5b2a285c612147bc8dbfe4e18b40a
danattali <danattali242@gmail.com>
2024-03-26 15:11:18 +0200

    new

--- src/routes/Cards.tsx
+++ src/routes/Cards.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const Cards = () => {
+    fetch("https://monkfish-app-z9uza.ondigitalocean.app/bcard2/cards")
+        .then((res) => res.json())
+        .then((json) => {
+            console.log(json);
+        })
     return (
         <div>Cards</div>
     )
