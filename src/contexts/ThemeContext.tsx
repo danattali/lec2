e449890c2161612d608dc208e249b9bf8ee43290
danattali <danattali242@gmail.com>
2024-03-26 10:38:00 +0200

    new

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,20 +1,17 @@
-import { createContext, useState } from "react";
-
-export const ThemeContext = createContext({
-    theme: "light",
-    toggle: () => { },
-});
+import { useState } from "react";
 
 export const ThemeProvider = ({ children }) => {
     const [theme, setTheme] = useState("light");
 
     const toggle = () => {
+        const newValue = theme == "light" ? "dark" : "light";
+
+        if (newValue === "dark") {
+            document.body.classList.add("dark");
+        } else {
+            document.body.classList.remove("dark");
+        }
+
         setTheme((prev) => (prev === "light" ? "dark" : "light"));
     };
-
-    return (
-        <ThemeContext.Provider value={{ theme, toggle }}>
-            {children}
-        </ThemeContext.Provider>
-    );
-};
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Register from "./routes/Register.tsx";
-import Login from "./routes/Login.tsx";
-import Cards from "./routes/Cards.tsx";
-import Error from "./routes/Error.tsx";
-import Root from "./layouts/Root.tsx";
-import Card from "./routes/Card.tsx";
-import Playground from "./routes/Playground.tsx";
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "./contexts/ThemeContext.tsx";
+import { router } from "./routes/router.tsx";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <Error />,
-    children: [
-      { path: "/register", element: <Register /> },
-      { path: "/login", element: <Login /> },
-      { path: "/cards", element: <Cards /> },
-      { path: "/cards/:id", element: <Card /> },
-      { path: "/Playground", element: <Playground /> },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
