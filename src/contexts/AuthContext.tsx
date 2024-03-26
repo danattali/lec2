d1f05b4ae86eccce114d3cfcf42983a2b63df96f
danattali <danattali242@gmail.com>
2024-03-26 12:30:56 +0200

    new

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,18 @@
-//save jwt
-//save is logged in
+import { createContext, useState } from "react";
 
+export const AuthContext = createContext({
+    isLoggedIn: false,
+    login: () => { },
+    logout: () => { },
+});
 
-//functions:
-//login
-//logout
+export const AuthContextProvider = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const login = () => { };
+    const logout = () => { };
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
