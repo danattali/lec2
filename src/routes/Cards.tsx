9d411026ec7bdda5746afdc2ecf653fb66373380
danattali <danattali242@gmail.com>
2024-04-06 12:27:56 +0300

    new

--- src/routes/Cards.tsx
+++ src/routes/Cards.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { CardType } from "../@types/types";
 import { getCards } from "../services/cards";
+import { Link } from "react-router-dom";
 import Spinners from "../components/Spinners/Spinners";
 
 
@@ -30,6 +31,21 @@ const Cards = () => {
         <div className="flex flex-col justify-center items-center">
             {loading && <Spinners />}
             {error && <div>{error}</div>}
-        </div>)
+
+            {cards.map((c) => (
+                <Link
+                    to={`cards/${c._id}`}
+                    key={c._id}
+                    className="shadow-2xl p-5 w-1/2 mx-auto rounded-md my-2 text-center"
+                >
+                    <h2 className="text-xl">{c.title}</h2>
+                    <hr />
+                    <p>{c.subtitle}</p>
+                    <img src={c.image.url} alt={c.image.alt} />
+                </Link>
+            ))}
+        </div>
+    );
 };
+
 export default Cards;
